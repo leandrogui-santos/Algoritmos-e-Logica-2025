d4d68319651c01711d6bf3422df6da201081aee1
Leandro Guilherme dos Santos <leandro.guilherme@msn.com>
2025-10-06 19:44:53 -0300

    Atividades Aula 09

---
+++ Ativ02_A09.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número inteiro: "))
+
+for i in range(1,11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
+
+print(f"Essa é a tabuada do {numero}!")
